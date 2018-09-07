d27eb032dbf96becbf2c101789aa6d43c63479b3
jojo <jojomickymack@gmail.com>
2018-09-06 20:44:55 -0600

    fixed to work with the public repo

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,8 @@ Content.create(
 	content_type: 'blog',
 	title: 'NWjs, Cordova, and Chrome',
 	long_title: 'desktop apps, web apps, and android apps from one www directory',
-	title_img_path: '/blog_data/web_apps/web_apps.png',
-	markdown_path: '/blog_data/web_apps/web_apps.md'
+	title_img_path: '/blog/web_apps/web_apps.png',
+	markdown_path: '/blog/web_apps/web_apps.md'
 )
 
 Content.create(
@@ -12,8 +12,8 @@ Content.create(
 	content_type: 'blog',
 	title: 'Building Php On A Chromebook',
 	long_title: 'Setting Up Cakephp and building php on a chromebook',
-	title_img_path: '/blog_data/build_php/build_php.png',
-	markdown_path: '/blog_data/build_php/build_php.md'
+	title_img_path: '/blog/build_php/build_php.png',
+	markdown_path: '/blog/build_php/build_php.md'
 )
 
 Content.create(
@@ -28,8 +28,8 @@ Content.create(
 	content_type: 'blog',
 	title: 'androidx86',
 	long_title: 'android x86 rocks and you should install it',
-	title_img_path: '/blog_data/androidx86/androidx86.png',
-	markdown_path: '/blog_data/androidx86/androidx86.md'
+	title_img_path: '/blog/androidx86/androidx86.png',
+	markdown_path: '/blog/androidx86/androidx86.md'
 )
 
 Content.create(
@@ -37,8 +37,8 @@ Content.create(
 	content_type: 'blog',
 	title: 'libktx platformer basics',
 	long_title: 'the components of a simple 2d platformer using tiled',
-	title_img_path: '/blog_data/libktx_platformer/libktx_platformer.png',
-	markdown_path: '/blog_data/libktx_platformer/libktx_platformer.md'
+	title_img_path: '/blog/libktx_platformer/libktx_platformer.png',
+	markdown_path: '/blog/libktx_platformer/libktx_platformer.md'
 )
 
 Content.create(
@@ -46,8 +46,8 @@ Content.create(
 	content_type: 'blog',
 	title: 'love2d',
 	long_title: 'love2d is an easy game development platform',
-	title_img_path: '/blog_data/love2d/love2d.png',
-	markdown_path: '/blog_data/love2d/love2d.md'
+	title_img_path: '/blog/love2d/love2d.png',
+	markdown_path: '/blog/love2d/love2d.md'
 )
 
 Content.create(
@@ -55,8 +55,8 @@ Content.create(
 	content_type: 'blog',
 	title: 'Installing And Linking To SDL2',
 	long_title: 'here\'s how get started with SDL2',
-	title_img_path: '/blog_data/cmake_sdl2/cmake_sdl2.png',
-	markdown_path: '/blog_data/cmake_sdl2/cmake_sdl2.md'
+	title_img_path: '/blog/cmake_sdl2/cmake_sdl2.png',
+	markdown_path: '/blog/cmake_sdl2/cmake_sdl2.md'
 )
 
 Content.create(
@@ -64,8 +64,8 @@ Content.create(
 	content_type: 'blog',
 	title: 'signing apks',
 	long_title: 'to distribute android apps, they must be signed',
-	title_img_path: '/blog_data/signing_apps/signed.png',
-	markdown_path: '/blog_data/signing_apps/signing_apps.md'
+	title_img_path: '/blog/signing_apps/signed.png',
+	markdown_path: '/blog/signing_apps/signing_apps.md'
 )
 
 Content.create(
@@ -73,8 +73,8 @@ Content.create(
 	content_type: 'blog',
 	title: 'firefox webcomponents',
 	long_title: 'firefox is finally enabling shadow dom',
-	title_img_path: '/blog_data/web/mdn_capture.png',
-	markdown_path: '/blog_data/web/web_components.md'
+	title_img_path: '/blog/web/mdn_capture.png',
+	markdown_path: '/blog/web/web_components.md'
 )
 
 Content.create(
@@ -82,8 +82,8 @@ Content.create(
 	content_type: 'blog',
 	title: 'chromebooks',
 	long_title: 'chromebooks have a lot of potential',
-	title_img_path: '/blog_data/chromebooks/chromebook.png',
-	markdown_path: '/blog_data/chromebooks/chromebooks.md'
+	title_img_path: '/blog/chromebooks/chromebook.png',
+	markdown_path: '/blog/chromebooks/chromebooks.md'
 )
 
 Content.create(
@@ -91,8 +91,8 @@ Content.create(
 	content_type: 'blog',
 	title: 'pico8',
 	long_title: 'pico8 is a really cool fantasy console',
-	title_img_path: '/blog_data/pico8/pico8.png',
-	markdown_path: '/blog_data/pico8/pico8.md'
+	title_img_path: '/blog/pico8/pico8.png',
+	markdown_path: '/blog/pico8/pico8.md'
 )
 
 Content.create(
@@ -100,8 +100,8 @@ Content.create(
 	content_type: 'blog',
 	title: 'ndk recursion',
 	long_title: 'getting started with the android ndk',
-	title_img_path: '/blog_data/ndk_recursive/recursion.png',
-	markdown_path: '/blog_data/ndk_recursive/ndk_recursive.md'
+	title_img_path: '/blog/ndk_recursive/recursion.png',
+	markdown_path: '/blog/ndk_recursive/ndk_recursive.md'
 )
 
 Content.create(
@@ -109,8 +109,8 @@ Content.create(
 	content_type: 'blog',
 	title: 'libgdx and kotlin',
 	long_title: 'libgdx and kotlin are good for android game development',
-	title_img_path: '/blog_data/libgdx_kotlin/libgdx_kotlin.png',
-	markdown_path: '/blog_data/libgdx_kotlin/libgdx_kotlin.md'
+	title_img_path: '/blog/libgdx_kotlin/libgdx_kotlin.png',
+	markdown_path: '/blog/libgdx_kotlin/libgdx_kotlin.md'
 )
 
 Content.create(
@@ -118,8 +118,8 @@ Content.create(
 	content_type: 'blog',
 	title: 'learning about go sorting and reflection',
 	long_title: 'go has great support for interfaces and reflection',
-	title_img_path: '/blog_data/go_sort/go_sort.jpg',
-	markdown_path: '/blog_data/go_sort/go_sort.md'
+	title_img_path: '/blog/go_sort/go_sort.jpg',
+	markdown_path: '/blog/go_sort/go_sort.md'
 )
 
 Content.create(
@@ -127,8 +127,8 @@ Content.create(
 	content_type: 'blog',
 	title: 'porting a cpp program to go',
 	long_title: 'checking out cpp, checking out go',
-	title_img_path: '/blog_data/cpp_to_go/golang.png',
-	markdown_path: '/blog_data/cpp_to_go/cpp_to_go.md'
+	title_img_path: '/blog/cpp_to_go/golang.png',
+	markdown_path: '/blog/cpp_to_go/cpp_to_go.md'
 )
 
 Content.create(
@@ -136,8 +136,8 @@ Content.create(
 	content_type: 'blog',
 	title: 'linked list in ruby',
 	long_title: 'linked lists are a basic way to organize data in memory',
-	title_img_path: '/blog_data/ruby_linked_list/linked_list.png',
-	markdown_path: '/blog_data/ruby_linked_list/linked_list.md'
+	title_img_path: '/blog/ruby_linked_list/linked_list.png',
+	markdown_path: '/blog/ruby_linked_list/linked_list.md'
 )
 
 Content.create(
@@ -145,8 +145,8 @@ Content.create(
 	content_type: 'blog',
 	title: 'using spotify web api for android ndk project',
 	long_title: 'sorting data from spotify is fun on android using cpp',
-	title_img_path: '/blog_data/ndk_spotify/ndk_spotify.png',
-	markdown_path: '/blog_data/ndk_spotify/ndk_spotify.md'
+	title_img_path: '/blog/ndk_spotify/ndk_spotify.png',
+	markdown_path: '/blog/ndk_spotify/ndk_spotify.md'
 )
 
 Content.create(
@@ -154,8 +154,8 @@ Content.create(
 	content_type: 'blog',
 	title: 'CMake And MinGW On Windows',
 	long_title: 'here\'s how to set up and use mingw and cmake on windows',
-	title_img_path: '/blog_data/cmake_mingw/linking.png',
-	markdown_path: '/blog_data/cmake_mingw/cmake_mingw.md'
+	title_img_path: '/blog/cmake_mingw/linking.png',
+	markdown_path: '/blog/cmake_mingw/cmake_mingw.md'
 )
 
 Content.create(
@@ -163,8 +163,8 @@ Content.create(
 	content_type: 'blog',
 	title: 'sound synthesis in c',
 	long_title: 'here are some libraries for sound science',
-	title_img_path: '/blog_data/cpp_sound/cpp_sound.png',
-	markdown_path: '/blog_data/cpp_sound/cpp_sound.md'
+	title_img_path: '/blog/cpp_sound/cpp_sound.png',
+	markdown_path: '/blog/cpp_sound/cpp_sound.md'
 )
 
 Content.create(
@@ -172,8 +172,8 @@ Content.create(
 	content_type: 'blog',
 	title: 'fibonacci and factorials in ruby',
 	long_title: 'some optimizations are really key to increasing recursive calls',
-	title_img_path: '/blog_data/ruby_fibonacci/fibonacci.jpg',
-	markdown_path: '/blog_data/ruby_fibonacci/ruby_fibonacci.md'
+	title_img_path: '/blog/ruby_fibonacci/fibonacci.jpg',
+	markdown_path: '/blog/ruby_fibonacci/ruby_fibonacci.md'
 )
 
 
@@ -219,7 +219,7 @@ Content.create(
 	url: 'games/physics02',
 	content_type: 'game',
 	title: 'phaser3 verts',
-	iframe_path: '/games/games/phaser/physics02/index.html'
+	iframe_path: '/games/phaser/physics02/index.html'
 )
 
 Content.create(
